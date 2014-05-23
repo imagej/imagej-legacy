da689a99206c15e6aa7af12b868957ad71cc7746
Mark Hiner <hinerm@gmail.com>
2014-05-23 11:11:48 -0500

    Add LegacyInputHarvester
    Needed a trivial LegacyInputHarvester. This can function the same as the
    SwingInputHarvester (since IJ1 and thus the LegacyUI is effectively a
    Swing UI). Just needed to tie it to the LegacyUI.

---
+++ src/main/java/net/imagej/legacy/ui/LegacyInputHarvester.java
@@ -0,0 +1,53 @@
+/*
+ * #%L
+ * SciJava UI components for Java Swing.
+ * %%
+ * Copyright (C) 2010 - 2014 Board of Regents of the University of
+ * Wisconsin-Madison.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.legacy.ui;
+
+import org.scijava.module.process.PreprocessorPlugin;
+import org.scijava.plugin.Plugin;
+import org.scijava.ui.swing.widget.SwingInputHarvester;
+import org.scijava.widget.InputHarvester;
+
+/**
+ * Trivial Legacy extension of the {@link SwingInputHarvester}. Just need to
+ * link it to the {@link LegacyUI}.
+ */
+@Plugin(type = PreprocessorPlugin.class, priority = InputHarvester.PRIORITY)
+public class LegacyInputHarvester extends SwingInputHarvester
+{
+
+	// -- Internal methods --
+
+	@Override
+	protected String getUI() {
+		return LegacyUI.NAME;
+	}
+
+}
