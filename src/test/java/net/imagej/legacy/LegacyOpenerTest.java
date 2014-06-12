ddb3af62d8d35b1ec08972f81d09d7797894c6c0
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-06-12 14:23:34 -0500

    Add a regression test verifying that open() in batch mode works
    This adds a simplified version of Paul van Schayck's macro, making
    use of imagej-legacy's own icon resources to verify that images can
    be loaded properly in headless mode, and that the particle analyzer
    manages to find the particles, too.
    
    The original mail thread describing the problem can be found here:
    
    	http://thread.gmane.org/gmane.comp.java.imagej/33046
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ src/test/java/net/imagej/legacy/LegacyOpenerTest.java
@@ -0,0 +1,78 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2014 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.legacy;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assume.assumeTrue;
+
+import java.net.URL;
+
+import org.junit.Test;
+import org.scijava.Context;
+import org.scijava.script.ScriptModule;
+import org.scijava.script.ScriptService;
+
+/**
+ * Verifies that the batch mode opening works.
+ * 
+ * @author Johannes Schindelin
+ */
+public class LegacyOpenerTest {
+	/**
+	 * This regression test is based on a macro provided by Paul van Schayck.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testPaulsMacro() throws Exception {
+		final URL url = getClass().getResource("/icons/imagej-256.png");
+		assertNotNull(url);
+		assumeTrue("file".equals(url.getProtocol()));
+		final String path = url.getPath();
+		final String macro = "// @OUTPUT int nResults\n"
+				+ "open('" + path + "');\n"
+				+ "if (nImages() != 1) exit('Oh no!');\n"
+				+ "run('8-bit');"
+				+ "setThreshold(150,255);"
+				+ "run('Analyze Particles...', 'size=20-Infinity circularity=0.40-1.00');\n";
+
+		final Context context = new Context();
+		final ScriptService script = context.getService(ScriptService.class);
+		assertNotNull(script);
+		final ScriptModule module = script.run("pauls-macro.ijm", macro, true).get();
+		final Integer nResults = (Integer) module.getOutput("nResults");
+		assertNotNull(nResults);
+		assertEquals(10, (int) nResults);
+		context.dispose();
+	}
+}
