3dedc32cb4609f2840db65947d6adedbcba29400
Curtis Rueden <ctrueden@wisc.edu>
2014-06-30 14:57:05 -0500

    Add integration test to verify shutdown behavior
    This test needs to use maven-invoker-plugin to run in a separate JVM,
    because ImageJ1 calls System.exit(0) upon successful shutdown. We really
    want to verify that indeed this action occurs.
    
    Note that maven-surefire-plugin and maven-failsafe-plugin do not support
    System.exit getting called from tests, so using maven-invoker-plugin is
    the best way to go here.

---
+++ src/it/ij1-quit/src/main/java/SystemExitIT.java
@@ -0,0 +1,95 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2014 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+import ij.IJ;
+import net.imagej.legacy.DefaultLegacyHooks;
+import net.imagej.legacy.DefaultLegacyService;
+import net.imagej.legacy.LegacyService;
+import net.imagej.legacy.ui.LegacyUI;
+import net.imagej.patcher.LegacyInjector;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.scijava.Context;
+import org.scijava.ui.UIService;
+import org.scijava.ui.UserInterface;
+
+/**
+ * Tests for verifying that ImageJ shuts down properly using
+ * {@code System.exit(0)} when {@link ij.ImageJ#quit()} is called.
+ * 
+ * @author Curtis Rueden
+ */
+public class SystemExitIT {
+
+	static {
+		LegacyInjector.preinit();
+	}
+
+	public void testSystemExit() throws Exception {
+		final Context context = new Context(LegacyService.class, UIService.class);
+
+		// verify that ImageJ1 is active
+		assertNotNull(IJ.getInstance());
+
+		// verify that the legacy layer is active
+		assertNotNull(DefaultLegacyService.getInstance());
+
+		// NB: Tell ImageJ1 to shut down the entire JVM when quitting.
+		IJ.getInstance().exitWhenQuitting(true);
+
+		// quit ImageJ1 the usual way, on a separate thread
+		IJ.getInstance().quit();
+
+		// wait for the JVM to terminate
+		final long timeout = 10; // wait at most this many seconds for IJ1 to quit
+		Thread.sleep(1000 * timeout);
+
+		// you didn't save my life, you ruined my death
+		fail("ImageJ1 failed to terminate the JVM after " + timeout + " seconds");
+	}
+
+	public static void main(final String... args) {
+		try {
+			final SystemExitIT systemExitIT = new SystemExitIT();
+			systemExitIT.testSystemExit();
+		}
+		catch (final Throwable t) {
+			t.printStackTrace(System.err);
+			System.exit(1);
+		}
+	}
+
+}
