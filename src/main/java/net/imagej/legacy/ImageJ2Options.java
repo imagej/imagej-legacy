968dc534a40d0ce8a349c0d2eec97a0b666d2ff4
Curtis Rueden <ctrueden@wisc.edu>
2014-08-15 09:48:16 -0500

    ImageJ2Options: split description using newlines
    Otherwise, Swing renders the tooltip all on one long line.

--- src/main/java/net/imagej/legacy/ImageJ2Options.java
+++ src/main/java/net/imagej/legacy/ImageJ2Options.java
@@ -88,11 +88,12 @@ public class ImageJ2Options extends OptionsPlugin implements Interactive
 	/**
 	 * If true, SCIFIO will be used during {@code File > Open} IJ1 calls.
 	 */
-	@Parameter(label = "Use SCIFIO when opening files",
-		description = "Whether to use ImageJ2's file I/O mechanism when opening "
-			+ "files. Image files will be opened using the SCIFIO library "
-			+ "(SCientific Image Format Input and Output), which provides truly "
-			+ "extensible support for reading and writing image file formats.",
+	@Parameter(
+		label = "Use SCIFIO when opening files",
+		description = "<html>Whether to use ImageJ2's file I/O mechanism when opening "
+			+ "files.<br>Image files will be opened using the SCIFIO library "
+			+ "(SCientific Image<br>Format Input and Output), which provides truly "
+			+ "extensible support for<br>reading and writing image file formats.",
 		callback = "run")
 	private boolean newStyleIO = false;
 
