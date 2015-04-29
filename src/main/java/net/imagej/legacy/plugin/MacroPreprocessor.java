02e9e46296a9fd44962b3cd7ccda12d53183e704
Curtis Rueden <ctrueden@wisc.edu>
2015-04-28 22:17:43 -0500

    Make all SciJava modules macro recordable
    This works by doing two things:
    
    1. Adds a preprocessor that, when the module is invoked from an ImageJ
       macro, populates and resolves all inputs from the IJ1 options string.
    
    2. Adds a postprocessor that tells the macro recorder (via
       Recorder.recordOption(key, value) calls) what the final input values
       of the module were.
    
    Together, they work brilliantly: you can now record any SciJava module
    via the IJ1 Macro Recorder, and then execute the recorded line of code
    to repeat the module execution with the same input values.
    
    This has the usual IJ1 caveats, of course:
    
    - The values must be expressible as strings and convertible back to
      their original types from said strings.
    
    - IJ1 string quoting uses brackets when there are spaces in the string;
      if there are also brackets in the string, things can fall down hard.
    
    I did work around the boolean issue, though, by explicitly passing
    "true" and "false" as the boolean values always. This works nicely, and
    also solves the case of Boolean parameters which have three states
    (true, false and null). The only time a parameter is left out of the
    macro options string is when the value is actually null.
    
    Thanks to Johannes Schindelin for spurring me to complete this work!

---
+++ src/main/java/net/imagej/legacy/plugin/MacroPreprocessor.java
@@ -0,0 +1,79 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2015 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.legacy.plugin;
+
+import ij.IJ;
+import ij.Macro;
+
+import org.scijava.Priority;
+import org.scijava.convert.ConvertService;
+import org.scijava.module.Module;
+import org.scijava.module.ModuleItem;
+import org.scijava.module.process.AbstractPreprocessorPlugin;
+import org.scijava.module.process.PreprocessorPlugin;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+/**
+ * Populates the module's input values, when the command is invoked from an
+ * ImageJ 1.x macro.
+ * 
+ * @author Curtis Rueden
+ */
+@Plugin(type = PreprocessorPlugin.class,
+	priority = 2 * Priority.VERY_HIGH_PRIORITY)
+public class MacroPreprocessor extends AbstractPreprocessorPlugin {
+
+	@Parameter
+	private ConvertService convertService;
+
+	// -- ModuleProcessor methods --
+
+	@Override
+	public void process(final Module module) {
+		if (!IJ.isMacro()) return;
+		final String options = Macro.getOptions();
+		for (final ModuleItem<?> input : module.getInfo().inputs()) {
+			final String name = input.getName();
+			final String value = Macro.getValue(options, name, null);
+			final Class<?> type = input.getType();
+			if (!convertService.supports(value, type)) {
+				// cannot convert macro value into the input's actual type
+				continue;
+			}
+			final Object converted = convertService.convert(value, type);
+			module.setInput(name, converted);
+			module.setResolved(name, true);
+		}
+	}
+
+}
