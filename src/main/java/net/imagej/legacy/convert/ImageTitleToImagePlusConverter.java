e62ba506da497e303bb6b59160598f71954701cd
Curtis Rueden <ctrueden@wisc.edu>
2015-08-26 16:45:28 -0500

    Make multiple ImagePlus inputs work as intended
    This uses (abuses?) the Scijava conversion framework to offer a pool of
    candidate ImageTitle objects, which are nothing more than the available
    collection of ImagePluses from the WindowManager, wrapped in an adapter
    class with suitable toString() method that returns the image title
    (which is what we want dropdown list boxes to display).

---
+++ src/main/java/net/imagej/legacy/convert/ImageTitleToImagePlusConverter.java
@@ -0,0 +1,122 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2015 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.legacy.convert;
+
+import ij.ImagePlus;
+import ij.WindowManager;
+
+import java.util.Collection;
+
+import org.scijava.convert.AbstractConverter;
+import org.scijava.convert.ConvertService;
+import org.scijava.convert.Converter;
+import org.scijava.plugin.Plugin;
+import org.scijava.widget.ObjectWidget;
+import org.scijava.widget.WidgetModel;
+
+/**
+ * Converts an image title to the corresponding {@link ImagePlus} object.
+ * <p>
+ * This converter is a hack to expose available {@link ImagePlus} objects as
+ * choices for the widget framework. It allows {@link ObjectWidget}
+ * implementations to display the available {@link ImagePlus} objects by title.
+ * </p>
+ * <p>
+ * Note that unlike most other classes in the ImageJ Legacy project, this one
+ * <em>needs</em> typed references to classes in the {@code ij} package,
+ * particularly {@link ImagePlus}, so that the conversion logic works as
+ * intended. It seems to work without side effects in the standard case...
+ * </p>
+ *
+ * @author Curtis Rueden
+ * @see ConvertService#getCompatibleInputs(Class)
+ * @see WidgetModel#getObjectPool()
+ */
+@Plugin(type = Converter.class)
+public class ImageTitleToImagePlusConverter extends
+	AbstractConverter<ImageTitleToImagePlusConverter.ImageTitle, ImagePlus>
+{
+
+	// -- Converter methods --
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public <T> T convert(final Object src, final Class<T> dest) {
+		return src instanceof ImageTitle ? (T) ((ImageTitle) src).imp : null;
+	}
+
+	@Override
+	public void populateInputCandidates(final Collection<Object> objects) {
+		final int[] imageIDs = WindowManager.getIDList();
+		if (imageIDs == null) return;
+		for (final int imageID : imageIDs) {
+			final ImagePlus imp = WindowManager.getImage(imageID);
+			if (imp != null) objects.add(new ImageTitle(imp));
+		}
+	}
+
+	@Override
+	public Class<ImagePlus> getOutputType() {
+		return ImagePlus.class;
+	}
+
+	@Override
+	public Class<ImageTitle> getInputType() {
+		return ImageTitle.class;
+	}
+
+	// -- Helper classes --
+
+	/**
+	 * Adapter for {@link ImagePlus} that emits the image title when
+	 * {@link #toString()} is called.
+	 *
+	 * @author Curtis Rueden
+	 */
+	public static class ImageTitle {
+
+		private final ImagePlus imp;
+
+		public ImageTitle(final ImagePlus imp) {
+			this.imp = imp;
+		}
+
+		// -- Object methods --
+
+		@Override
+		public String toString() {
+			return imp.getTitle();
+		}
+
+	}
+
+}
