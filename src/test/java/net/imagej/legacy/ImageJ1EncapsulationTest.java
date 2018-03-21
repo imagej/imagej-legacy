90607bb7433e77519add8a87f458280693d44348
Alison Walter <awalter2@wisc.edu>
2018-03-21 11:35:21 -0500

    Add converters for ImageJ 1.x ROIs to Imglib2

--- src/test/java/net/imagej/legacy/ImageJ1EncapsulationTest.java
+++ src/test/java/net/imagej/legacy/ImageJ1EncapsulationTest.java
@@ -102,15 +102,23 @@ public class ImageJ1EncapsulationTest {
 					className.startsWith(net.imagej.legacy.convert.roi.AbstractPolygonRoiWrapper.class.getName()) ||
 					className.startsWith(net.imagej.legacy.convert.roi.AbstractRoiToMaskPredicateConverter.class.getName()) ||
 					className.startsWith(net.imagej.legacy.convert.roi.DefaultRoiWrapper.class.getName()) ||
+					className.startsWith(net.imagej.legacy.convert.roi.IJLineToLineConverter.class.getName()) ||
 					className.startsWith(net.imagej.legacy.convert.roi.IJRealRoiWrapper.class.getName()) ||
 					className.startsWith(net.imagej.legacy.convert.roi.IJRoiWrapper.class.getName()) ||
 					className.startsWith(net.imagej.legacy.convert.roi.IrregularPolylineRoiWrapper.class.getName()) ||
 					className.startsWith(net.imagej.legacy.convert.roi.LineWrapper.class.getName()) ||
+					className.startsWith(net.imagej.legacy.convert.roi.OvalRoiToEllipsoidConverter.class.getName()) ||
 					className.startsWith(net.imagej.legacy.convert.roi.OvalRoiWrapper.class.getName()) ||
+					className.startsWith(net.imagej.legacy.convert.roi.PointRoiToRealPointCollectionConverter.class.getName()) ||
 					className.startsWith(net.imagej.legacy.convert.roi.PointRoiWrapper.class.getName()) ||
+					className.startsWith(net.imagej.legacy.convert.roi.PolygonRoiToPolygon2DConverter.class.getName()) ||
 					className.startsWith(net.imagej.legacy.convert.roi.PolygonRoiWrapper.class.getName()) ||
+					className.startsWith(net.imagej.legacy.convert.roi.PolylineRoiToPolylineConverter.class.getName()) ||
+					className.startsWith(net.imagej.legacy.convert.roi.PolylineRoiToRealMaskRealIntervalConverter.class.getName()) ||
 					className.startsWith(net.imagej.legacy.convert.roi.PolylineRoiWrapper.class.getName()) ||
+					className.startsWith(net.imagej.legacy.convert.roi.RoiToBoxConverter.class.getName()) ||
 					className.startsWith(net.imagej.legacy.convert.roi.RoiWrapper.class.getName()) ||
+					className.startsWith(net.imagej.legacy.convert.roi.ShapeRoiToMaskRealIntervalConverter.class.getName()) ||
 					className.startsWith(net.imagej.legacy.convert.roi.ShapeRoiWrapper.class.getName()) ||
 					className.startsWith(net.imagej.legacy.convert.roi.UnmodifiablePolygonRoiWrapper.class.getName()) ||
 					className.startsWith(net.imagej.legacy.convert.roi.UnmodifiablePolylineRoiWrapper.class.getName()) ||
