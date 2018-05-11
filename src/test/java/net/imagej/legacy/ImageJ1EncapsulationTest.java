e52ce6dec0c63212dafffb3d0b70642aa9b162e6
Alison Walter <awalter2@wisc.edu>
2018-05-11 16:20:15 -0500

    Add converter to unwrap TableWrapper

--- src/test/java/net/imagej/legacy/ImageJ1EncapsulationTest.java
+++ src/test/java/net/imagej/legacy/ImageJ1EncapsulationTest.java
@@ -102,6 +102,7 @@ public class ImageJ1EncapsulationTest {
 					className.startsWith(net.imagej.legacy.convert.ResultsTableWrapper.class.getName()) ||
 					className.startsWith(net.imagej.legacy.convert.StringToImagePlusConverter.class.getName()) ||
 					className.startsWith(net.imagej.legacy.convert.TableToResultsTableConverters.class.getName()) ||
+					className.startsWith(net.imagej.legacy.convert.TableUnwrapper.class.getName()) ||
 					className.startsWith(net.imagej.legacy.convert.TableWrapper.class.getName()) ||
 					className.startsWith(net.imagej.legacy.convert.roi.AbstractMaskPredicateToRoiConverter.class.getName()) ||
 					className.startsWith(net.imagej.legacy.convert.roi.AbstractPolygonRoiWrapper.class.getName()) ||
