70f37f012e0ad15af9a5676a14cf0b42b3c1edb4
Curtis Rueden <ctrueden@wisc.edu>
2018-06-25 15:56:18 -0500

    Clean up style; add license headers

--- src/main/java/net/imagej/legacy/plugin/MacroAutoCompletionProvider.java
+++ src/main/java/net/imagej/legacy/plugin/MacroAutoCompletionProvider.java
@@ -1,10 +1,37 @@
+/*-
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2018 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
 package net.imagej.legacy.plugin;
 
-import org.fife.ui.autocomplete.BasicCompletion;
-import org.fife.ui.autocomplete.Completion;
-import org.fife.ui.autocomplete.DefaultCompletionProvider;
-import org.fife.ui.rtextarea.RTextArea;
-import org.fife.ui.rtextarea.ToolTipSupplier;
+import ij.IJ;
 
 import java.awt.event.MouseEvent;
 import java.io.BufferedReader;
@@ -15,156 +42,171 @@ import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.List;
 
+import org.fife.ui.autocomplete.BasicCompletion;
+import org.fife.ui.autocomplete.Completion;
+import org.fife.ui.autocomplete.DefaultCompletionProvider;
+import org.fife.ui.rtextarea.RTextArea;
+import org.fife.ui.rtextarea.ToolTipSupplier;
+
 /**
- * The MacroAutoCompletionProvider creates the list of auto completion suggestions from html
- * file in the resources directory
+ * Creates the list of auto-completion suggestions from functions.html
+ * documentation.
  *
- * Author: @haesleinhuepf
- * 06 2018
+ * @author Robert Haase
  */
-class MacroAutoCompletionProvider extends DefaultCompletionProvider implements ToolTipSupplier {
-    private static MacroAutoCompletionProvider instance = null;
+class MacroAutoCompletionProvider extends DefaultCompletionProvider implements
+	ToolTipSupplier
+{
 
-    private MacroAutoCompletionProvider() {
-        if (!parseFunctionsHtmlDoc("https://imagej.net/developer/macro/functions.html")) {
-            parseFunctionsHtmlDoc("/doc/ij1macro/functions.html");
-        }
-        parseFunctionsHtmlDoc("/doc/ij1macro/functions_extd.html");
-    }
+	private static MacroAutoCompletionProvider instance = null;
 
-    public static synchronized MacroAutoCompletionProvider getInstance() {
-        if (instance == null) {
-            instance = new MacroAutoCompletionProvider();
-        }
-        return instance;
-    }
+	private MacroAutoCompletionProvider() {
+		if (!parseFunctionsHtmlDoc(
+			"https://imagej.net/developer/macro/functions.html"))
+		{
+			parseFunctionsHtmlDoc("/doc/ij1macro/functions.html");
+		}
+		parseFunctionsHtmlDoc("/doc/ij1macro/functions_extd.html");
+	}
 
-    private boolean parseFunctionsHtmlDoc(String filename) {
-        InputStream resourceAsStream;
+	public static synchronized MacroAutoCompletionProvider getInstance() {
+		if (instance == null) {
+			instance = new MacroAutoCompletionProvider();
+		}
+		return instance;
+	}
 
-        try {
-            if (filename.startsWith("http")) {
-                URL url = new URL(filename);
-                resourceAsStream = url.openStream();
-            } else{
+	private boolean parseFunctionsHtmlDoc(final String filename) {
+		InputStream resourceAsStream;
 
-                ClassLoader classLoader = getClass().getClassLoader();
-                resourceAsStream = classLoader.getResourceAsStream(filename);
-            }
-            if (resourceAsStream == null) return false;
-            BufferedReader br
-                    = new BufferedReader(new InputStreamReader(resourceAsStream));
+		try {
+			if (filename.startsWith("http")) {
+				final URL url = new URL(filename);
+				resourceAsStream = url.openStream();
+			}
+			else {
+				resourceAsStream = getClass().getResourceAsStream(filename);
+			}
+			if (resourceAsStream == null) return false;
+			final BufferedReader br = //
+				new BufferedReader(new InputStreamReader(resourceAsStream));
 
-            String name = "";
-            String headline = "";
-            String description = "";
-            String line;
-            while ((line = br.readLine()) != null) {
-                line = line.trim();
-                line = line.
-                        replace("<a name=\"", "<a name=").
-                        replace("\"></a>", "></a>");
-                if (line.contains("<a name=")) {
-                    if (checkName(name)) {
-                        addCompletion(makeListEntry(this, headline, name, description));
-                    }
-                    name = htmlToText(line.
-                            split("<a name=")[1].
-                            split("></a>")[0]);
-                    description = "";
-                    headline = "";
-                } else {
-                    if (headline.length() == 0) {
-                        headline = htmlToText(line);
-                    } else {
-                        description = description + line + "\n";
-                    }
-                }
+			String name = "";
+			String headline = "";
+			String description = "";
+			String line;
+			while ((line = br.readLine()) != null) {
+				line = line.trim();
+				line = line.replace("<a name=\"", "<a name=").replace("\"></a>",
+					"></a>");
+				if (line.contains("<a name=")) {
+					if (checkName(name)) {
+						addCompletion(makeListEntry(this, headline, name, description));
+					}
+					name = htmlToText(line.split("<a name=")[1].split("></a>")[0]);
+					description = "";
+					headline = "";
+				}
+				else {
+					if (headline.length() == 0) {
+						headline = htmlToText(line);
+					}
+					else {
+						description = description + line + "\n";
+					}
+				}
 
-            }
-            if (checkName(name)) {
-                addCompletion(makeListEntry(this, headline, name, description));
-            }
+			}
+			if (checkName(name)) {
+				addCompletion(makeListEntry(this, headline, name, description));
+			}
 
-        } catch (UnknownHostException e) {
-            return false;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
+		}
+		catch (final UnknownHostException e) {
+			e.printStackTrace();
+			return false;
+		}
+		catch (final IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
 
-    private boolean checkName(String name) {
-        return (name.length() > 1) &&
-                (!name.trim().startsWith("<")) &&
-                (!name.trim().startsWith("-")) &&
-                (name.compareTo("Top") != 0) &&
-                (name.compareTo("IJ") != 0) &&
-                (name.compareTo("Stack") != 0) &&
-                (name.compareTo("Array") != 0) &&
-                (name.compareTo("file") != 0) &&
-                (name.compareTo("Fit") != 0) &&
-                (name.compareTo("List") != 0) &&
-                (name.compareTo("Overlay") != 0) &&
-                (name.compareTo("Plot") != 0) &&
-                (name.compareTo("Roi") != 0) &&
-                (name.compareTo("String") != 0) &&
-                (name.compareTo("Table") != 0) &&
-                (name.compareTo("Ext") != 0) &&
-                (name.compareTo("ext") != 0)&&
-                (name.compareTo("alphabar") != 0)&&
-                (name.compareTo("ext") != 0)
-                ;
-    }
+	private boolean checkName(final String name) {
+		return name.length() > 1 && //
+			!name.trim().startsWith("<") && //
+			!name.trim().startsWith("-") && //
+			name.compareTo("Top") != 0 && //
+			name.compareTo("IJ") != 0 && //
+			name.compareTo("Stack") != 0 && //
+			name.compareTo("Array") != 0 && //
+			name.compareTo("file") != 0 && //
+			name.compareTo("Fit") != 0 && //
+			name.compareTo("List") != 0 && //
+			name.compareTo("Overlay") != 0 && //
+			name.compareTo("Plot") != 0 && //
+			name.compareTo("Roi") != 0 && //
+			name.compareTo("String") != 0 && //
+			name.compareTo("Table") != 0 && //
+			name.compareTo("Ext") != 0 && //
+			name.compareTo("ext") != 0 && //
+			name.compareTo("alphabar") != 0 && //
+			name.compareTo("ext") != 0;
+	}
 
-    private String htmlToText(String text) {
-        return text.
-                replace("&quot;", "\"").
-                replace("<b>", "").
-                replace("</b>", "").
-                replace("<i>", "").
-                replace("</i>", "").
-                replace("<br>", "");
-    }
+	private String htmlToText(final String text) {
+		return text //
+			.replace("&quot;", "\"") //
+			.replace("<b>", "") //
+			.replace("</b>", "") //
+			.replace("<i>", "") //
+			.replace("</i>", "") //
+			.replace("<br>", "");
+	}
 
-    private BasicCompletion makeListEntry(MacroAutoCompletionProvider provider, String headline, String name, String description) {
-        String link = "https://imagej.net/developer/macro/functions.html#" + name;
+	private BasicCompletion makeListEntry(
+		final MacroAutoCompletionProvider provider, final String headline,
+		final String name, String description)
+	{
+		final String link = //
+			"https://imagej.net/developer/macro/functions.html#" + name;
 
-        description = "<a href=\"" + link + "\">" + headline + "</a><br>" + description;
+		description = //
+			"<a href=\"" + link + "\">" + headline + "</a><br>" + description;
 
-        return new BasicCompletion(provider, headline, "", description);
-    }
+		return new BasicCompletion(provider, headline, "", description);
+	}
 
-    /**
-     * Returns the tool tip to display for a mouse event.<p>
-     *
-     * For this method to be called, the <tt>RSyntaxTextArea</tt> must be
-     * registered with the <tt>javax.swing.ToolTipManager</tt> like so:
-     *
-     * <pre>
-     * ToolTipManager.sharedInstance().registerComponent(textArea);
-     * </pre>
-     *
-     * @param textArea The text area.
-     * @param e The mouse event.
-     * @return The tool tip text, or <code>null</code> if none.
-     */
-    @Override
-    public String getToolTipText(RTextArea textArea, MouseEvent e) {
+	/**
+	 * Returns the tool tip to display for a mouse event.
+	 * <p>
+	 * For this method to be called, the <tt>RSyntaxTextArea</tt> must be
+	 * registered with the <tt>javax.swing.ToolTipManager</tt> like so:
+	 * </p>
+	 *
+	 * <pre>
+	 * ToolTipManager.sharedInstance().registerComponent(textArea);
+	 * </pre>
+	 *
+	 * @param textArea The text area.
+	 * @param e The mouse event.
+	 * @return The tool tip text, or <code>null</code> if none.
+	 */
+	@Override
+	public String getToolTipText(final RTextArea textArea, final MouseEvent e) {
 
-        String tip = null;
+		String tip = null;
 
-        List<Completion> completions = getCompletionsAt(textArea, e.getPoint());
-        if (completions!=null && completions.size()>0) {
-            // Only ever 1 match for us in C...
-            Completion c = completions.get(0);
-            tip = c.getToolTipText();
-        }
+		final List<Completion> completions = //
+			getCompletionsAt(textArea, e.getPoint());
+		if (completions != null && completions.size() > 0) {
+			// Only ever 1 match for us in C...
+			final Completion c = completions.get(0);
+			tip = c.getToolTipText();
+		}
 
-        return tip;
-
-    }
-
+		return tip;
+	}
 
 }
--- src/main/java/net/imagej/legacy/plugin/MacroLanguageSupportPlugin.java
+++ src/main/java/net/imagej/legacy/plugin/MacroLanguageSupportPlugin.java
@@ -1,121 +1,149 @@
+/*-
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2018 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
 package net.imagej.legacy.plugin;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+
+import javax.swing.SwingUtilities;
+
 import org.fife.rsta.ac.AbstractLanguageSupport;
-import org.fife.ui.autocomplete.*;
+import org.fife.ui.autocomplete.AutoCompletion;
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
-import org.fife.ui.rtextarea.RTextArea;
-import org.fife.ui.rtextarea.ToolTipSupplier;
 import org.scijava.plugin.Plugin;
 import org.scijava.ui.swing.script.LanguageSupportPlugin;
 
-import javax.swing.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
- * MacroLanguageSupportPlugin
+ * {@link LanguageSupportPlugin} making basic auto-completion available for IJ
+ * macro scripts. It offers all commands and additional help copied from the
+ * <a href="https://imagej.net/developer/macro/functions.html">ImageJ macro
+ * functions documentation</a>.
  *
- * This plugin makes basic auto completion available for IJ macro scripts. It offers
- * all commands and additional help copied from this website:
- *
- * https://imagej.net/developer/macro/functions.html
- *
- * Author: @haesleinhuepf
- * June 2018
+ * @author Robert Haase
  */
 @Plugin(type = LanguageSupportPlugin.class)
-public class MacroLanguageSupportPlugin extends AbstractLanguageSupport implements
-        LanguageSupportPlugin {
+public class MacroLanguageSupportPlugin extends AbstractLanguageSupport
+	implements LanguageSupportPlugin
+{
 
+	@Override
+	public String getLanguageName() {
+		return "IJ1 Macro";
+	}
 
-    @Override
-    public String getLanguageName() {
-        return "IJ1 Macro";
-    }
+	@Override
+	public void install(final RSyntaxTextArea rSyntaxTextArea) {
+		final AutoCompletion ac = createAutoCompletion(MacroAutoCompletionProvider
+			.getInstance());
+		ac.setAutoActivationDelay(100);
+		ac.setAutoActivationEnabled(true);
+		ac.setShowDescWindow(true);
+		ac.install(rSyntaxTextArea);
+		installImpl(rSyntaxTextArea, ac);
 
-    @Override
-    public void install(RSyntaxTextArea rSyntaxTextArea) {
-        AutoCompletion ac = createAutoCompletion(MacroAutoCompletionProvider.getInstance());
-        ac.setAutoActivationDelay(100);
-        ac.setAutoActivationEnabled(true);
-        ac.setShowDescWindow(true);
-        ac.install(rSyntaxTextArea);
-        installImpl(rSyntaxTextArea, ac);
+		rSyntaxTextArea.addKeyListener(new MacroAutoCompletionKeyListener(ac,
+			rSyntaxTextArea));
 
-        rSyntaxTextArea.addKeyListener(new MacroAutoCompletionKeyListener(ac, rSyntaxTextArea));
+		rSyntaxTextArea.setToolTipSupplier(MacroAutoCompletionProvider
+			.getInstance());
+	}
 
-        rSyntaxTextArea.setToolTipSupplier(MacroAutoCompletionProvider.getInstance());
-    }
+	@Override
+	public void uninstall(final RSyntaxTextArea rSyntaxTextArea) {
+		uninstallImpl(rSyntaxTextArea);
 
+		final ArrayList<KeyListener> toRemove = new ArrayList<>();
+		for (final KeyListener keyListener : rSyntaxTextArea.getKeyListeners()) {
+			if (keyListener instanceof MacroAutoCompletionKeyListener) {
+				toRemove.add(keyListener);
+			}
+		}
+		for (final KeyListener keyListener : toRemove) {
+			rSyntaxTextArea.removeKeyListener(keyListener);
+		}
 
-    @Override
-    public void uninstall(RSyntaxTextArea rSyntaxTextArea) {
-        uninstallImpl(rSyntaxTextArea);
+	}
 
-        ArrayList<KeyListener> toRemove = new ArrayList<>();
-        for (KeyListener keyListener : rSyntaxTextArea.getKeyListeners()) {
-            if (keyListener instanceof MacroAutoCompletionKeyListener) {
-                toRemove.add(keyListener);
-            }
-        }
-        for (KeyListener keyListener : toRemove) {
-            rSyntaxTextArea.removeKeyListener(keyListener);
-        }
+	private class MacroAutoCompletionKeyListener implements KeyListener {
 
-    }
+		AutoCompletion ac;
+		RSyntaxTextArea textArea;
+		ArrayList<Character> disabledChars;
 
+		public MacroAutoCompletionKeyListener(final AutoCompletion ac,
+			final RSyntaxTextArea textArea)
+		{
+			this.ac = ac;
+			this.textArea = textArea;
 
+			disabledChars = new ArrayList<>();
+			disabledChars.add(' ');
+			disabledChars.add('\n');
+			disabledChars.add('\t');
+			disabledChars.add(';');
+		}
 
-    private class MacroAutoCompletionKeyListener implements KeyListener {
-        AutoCompletion ac;
-        RSyntaxTextArea textArea;
-        ArrayList<Character> disabledChars;
+		@Override
+		public void keyTyped(final KeyEvent e) {
 
-        public MacroAutoCompletionKeyListener(AutoCompletion ac, RSyntaxTextArea textArea) {
-            this.ac = ac;
-            this.textArea = textArea;
+		}
 
-            disabledChars = new ArrayList<Character>();
-            disabledChars.add(' ');
-            disabledChars.add('\n');
-            disabledChars.add('\t');
-            disabledChars.add(';');
-        }
+		@Override
+		public void keyPressed(final KeyEvent e) {
 
-        @Override
-        public void keyTyped(KeyEvent e) {
+		}
 
-        }
-
-        @Override
-        public void keyPressed(KeyEvent e) {
-
-        }
-
-        @Override
-        public void keyReleased(KeyEvent e) {
-            SwingUtilities.invokeLater(()->{
-                if (disabledChars.contains(e.getKeyChar())) {
-                    if (!e.isControlDown()) { // the pulldown should not be hidden if CTRL+SPACE are pressed
-                        ac.hideChildWindows();
-                    }
-                } else if (
-                             e.getKeyCode() >= 65 // a
-                          && e.getKeyCode() <= 90 // z
-                        )
-                {
-                    if (MacroAutoCompletionProvider.getInstance().getAlreadyEnteredText(textArea).length() == 2 &&
-                        MacroAutoCompletionProvider.getInstance().getCompletions(textArea).size() != 1) {
-                        ac.doCompletion();
-                    }
-                }
-            });
-        }
-    }
-
+		@Override
+		public void keyReleased(final KeyEvent e) {
+			SwingUtilities.invokeLater(() -> {
+				if (disabledChars.contains(e.getKeyChar())) {
+					if (!e.isControlDown()) {
+						// the pulldown should not be hidden if CTRL+SPACE are pressed
+						ac.hideChildWindows();
+					}
+				}
+				else if (e.getKeyCode() >= 65 // a
+				&& e.getKeyCode() <= 90 // z
+				) {
+					if (MacroAutoCompletionProvider.getInstance().getAlreadyEnteredText(
+						textArea).length() == 2 && MacroAutoCompletionProvider.getInstance()
+							.getCompletions(textArea).size() != 1)
+					{
+						ac.doCompletion();
+					}
+				}
+			});
+		}
+	}
 
 }
