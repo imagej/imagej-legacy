7d05f322071528095d316998879e7dabbbbcff80
Matthias Arzt <arzt@mpi-cbg.de>
2022-03-16 14:49:26 +0100

    BatchModeTest: don't use the graphics environment for testing.
    The test now could also work without a graphics environment. But the truth
    is, that it's the legacy UI batch mode work with SciJava commends. The test
    fails if the legacy UI is not visible. This should ideally be fixed in the
    future.

--- src/test/java/net/imagej/legacy/BatchModeTest.java
+++ src/test/java/net/imagej/legacy/BatchModeTest.java
@@ -1,3 +1,31 @@
+/*
+ * #%L
+ * ImageJ2 software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2022 ImageJ2 developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
 package net.imagej.legacy;
 
 import ij.IJ;
@@ -18,7 +46,7 @@ import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import org.scijava.ui.UIService;
 
-import java.awt.*;
+import java.awt.GraphicsEnvironment;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assume.assumeFalse;
@@ -26,6 +54,8 @@ import static org.junit.Assume.assumeFalse;
 /**
  * Test if a {@link Command} that returns a {@link Dataset} behaves as expected
  * when a IJ1 macro is run in batch mode.
+ *
+ * @author Matthias Arzt
  */
 public class BatchModeTest {
 
@@ -37,12 +67,7 @@ public class BatchModeTest {
 
 	@Before
 	public void setUp() {
-		// only run the tests if a graphics environment is present
-		assumeFalse(GraphicsEnvironment.isHeadless());
-
 		context = new Context();
-		UIService service = context.service(UIService.class);
-		service.showUI();
 	}
 
 	@After
@@ -56,31 +81,39 @@ public class BatchModeTest {
 	public void testIJ1() {
 		WindowManager.closeAllWindows();
 		IJ.runMacro(
-			"setBatchMode(true);" +
-			"newImage(\"imageA\", \"8-bit black\", 2, 2, 1);" +
-			"newImage(\"imageB\", \"8-bit black\", 2, 2, 1);" +
-			"setBatchMode(\"exit & display\");"
+			"setBatchMode(true);\n" +
+			"newImage(\"imageA\", \"8-bit black\", 2, 2, 1);\n" +
+			"newImage(\"imageB\", \"8-bit black\", 2, 2, 1);\n" +
+			"run(\"Record Image Titles\");" +
+			"setBatchMode(false);"
 		);
-		assertArrayEquals(new String[] {"imageA", "imageB"}, WindowManager.getImageTitles());
+		assertArrayEquals(new String[] {"imageA", "imageB"}, RecordImageTitles.titles);
 	}
 
 	@Test
 	public void testBatchMode() {
+		showLegacyUI();
+
 		WindowManager.closeAllWindows();
 		IJ.runMacro(
 			"setBatchMode(true);" +
 			"run(\"Output Dataset\", \"title=imageA\");" +
 			"run(\"Output Dataset\", \"title=imageB\");" +
-			"setBatchMode(\"exit & display\");"
+			"run(\"Record Image Titles\");" +
+			"setBatchMode(false);"
 		);
-		assertArrayEquals(new String[] {"imageA", "imageB"}, WindowManager.getImageTitles());
+		assertArrayEquals(new String[] {"imageA", "imageB"}, RecordImageTitles.titles);
 	}
 
-	/**
-	 * Simple command that outputs a {@link Dataset}. Used for testing.
-	 *
-	 * @author Matthias Arzt
-	 */
+	private void showLegacyUI() {
+		// NB: The test fails, when the legacy ui is not visible.
+		// TODO: This should be improved. The test should also work in headless mode, without the legacy ui visible.
+		assumeFalse(GraphicsEnvironment.isHeadless());
+		UIService service = context.service(UIService.class);
+		service.showUI();
+	}
+
+	/** Simple command that outputs a {@link Dataset}. Used for testing. */
 	@Plugin(type = Command.class, menuPath = "ImageJ Legacy Test > Output Dataset")
 	public static class OutputDataset implements Command {
 
@@ -101,4 +134,15 @@ public class BatchModeTest {
 		}
 	}
 
+	/** Command the stores all image titles in a static field. Used for testing. */
+	@Plugin(type = Command.class, menuPath = "ImageJ Legacy Test > Record Image Titles")
+	public static class RecordImageTitles implements Command {
+
+		private static String[] titles;
+
+		@Override
+		public void run() {
+			titles = WindowManager.getImageTitles();
+		}
+	}
 }
